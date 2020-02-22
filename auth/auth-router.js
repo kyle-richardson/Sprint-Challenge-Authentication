aa4702dde8cd6cbeca0785b137f4a5e7394bd2ed
Kyle Richardson <kyle.richardson1@gmail.com>
2020-02-21 20:21:52 -0600

    basic app setup for auth

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,30 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const db = require("./auth-model");
 
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   // implement registration
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  db.findBy({ username })
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.user = user;
+
+        res.status(201).json({
+          message: `Welcome ${user.username}!`
+        });
+      } else {
+        res.status(401).json({ message: "You shall not pass!" });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: "failed to sign in", error: err.message });
+    });
 });
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,20 @@
   before granting access to the next middleware/route handler
 */
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+const restricted = (req, res, next) => {
+  if (req.session && req.session.user)
+    if (req.params.id) {
+      if (req.session.user.id == req.params.id)
+        //the user logged in can only operate on their own account (delete, access content)
+        next();
+      else
+        res
+          .status(401)
+          .json({
+            message: "You do not have priviledge to access or edit this user."
+          });
+    } else next();
+  else res.status(401).json({ message: "You shall not pass!" });
 };
+
+module.exports = restricted;
